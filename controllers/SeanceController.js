f72e93b255ef567240e198a548c7214ea2b90fe7
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:55 +0100

    Update SeanceController to manage seance-related operations including scheduling and updates.

--- controllers/SeanceController.js
+++ controllers/SeanceController.js
@@ -1,29 +1,31 @@
-const Seance = require('../models/Seance');
+const Seance = require("../models/Seance");
 
 // Ajouter une séance
 exports.addSeance = async (req, res) => {
-    const { film, salle, horaire, tarif } = req.body;
-    const seance = new Seance({ film, salle, horaire, tarif });
-    await seance.save();
-    res.status(201).json(seance);
+  const { film, salle, horaire, tarif } = req.body;
+  const seance = new Seance({ film, salle, horaire, tarif });
+  await seance.save();
+  res.status(201).json(seance);
 };
 
 // Lister les séances
 exports.getSeances = async (req, res) => {
-    const seances = await Seance.find().populate('film salle');
-    res.json(seances);
-};
+  const seances = await Seance.find().populate("film salle");
+  res.json(seances);
+};F
 
 // Modifier une séance
 exports.updateSeance = async (req, res) => {
-    const { id } = req.params;
-    const updatedSeance = await Seance.findByIdAndUpdate(id, req.body, { new: true });
-    res.json(updatedSeance);
+  const { id } = req.params;
+  const updatedSeance = await Seance.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.json(updatedSeance);
 };
 
 // Supprimer une séance
 exports.deleteSeance = async (req, res) => {
-    const { id } = req.params;
-    await Seance.findByIdAndDelete(id);
-    res.status(204).send();
+  const { id } = req.params;
+  await Seance.findByIdAndDelete(id);
+  res.status(204).send();
 };
