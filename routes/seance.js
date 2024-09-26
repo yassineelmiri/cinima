bdd465829097203845f043430d283e7b90d56596
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:24:00 +0100

    Add routes for seance management including scheduling and session updates.

--- routes/seance.js
+++ routes/seance.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const SeanceController = require('../controllers/SeanceController');
-const authMiddleware = require('../middleware/auth');
+const SeanceController = require("../controllers/SeanceController");
+const authMiddleware = require("../middleware/auth");
 
-router.post('/', SeanceController.addSeance);
-router.get('/', SeanceController.getSeances);
-router.put('/:id', SeanceController.updateSeance);
-router.delete('/:id', SeanceController.deleteSeance);
+router.post("/", SeanceController.addSeance);
+router.get("/", SeanceController.getSeances);
+router.put("/:id", SeanceController.updateSeance);
+router.delete("/:id", SeanceController.deleteSeance);
 
 module.exports = router;
