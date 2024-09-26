1634a37a7cecf30ff1e942cc7030ad75bb03a1fb
yassineelmiri <yassireelhima@gmail.com>
2024-09-26 23:53:18 +0100

    Add FilmController for managing films, including CRUD operations.

--- controllers/FilmController.js
+++ controllers/FilmController.js
@@ -1,29 +1,60 @@
-const Film = require('../models/Film');
+const Film = require("../models/Film");
 
-// Ajouter un film
+
 exports.addFilm = async (req, res) => {
-    const { titre, description, duree, genre, dateSortie } = req.body;
+  const { titre, description, duree, genre, dateSortie } = req.body;
+  try {
     const film = new Film({ titre, description, duree, genre, dateSortie });
     await film.save();
     res.status(201).json(film);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de l'ajout du film",
+      error: error.message,
+    });
+  }
 };
 
-// Lister les films
+
 exports.getFilms = async (req, res) => {
+  try {
     const films = await Film.find();
     res.json(films);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la récupération des films",
+      error: error.message,
+    });
+  }
 };
 
-// Modifier un film
 exports.updateFilm = async (req, res) => {
-    const { id } = req.params;
-    const updatedFilm = await Film.findByIdAndUpdate(id, req.body, { new: true });
+  const { id } = req.params;
+  try {
+    const updatedFilm = await Film.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedFilm)
+      return res.status(404).json({ message: "Film non trouvé" });
     res.json(updatedFilm);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la mise à jour du film",
+      error: error.message,
+    });
+  }
 };
 
-// Supprimer un film
 exports.deleteFilm = async (req, res) => {
-    const { id } = req.params;
-    await Film.findByIdAndDelete(id);
+  const { id } = req.params;
+  try {
+    const film = await Film.findByIdAndDelete(id);
+    if (!film) return res.status(404).json({ message: "Film non trouvé" });
     res.status(204).send();
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la suppression du film",
+      error: error.message,
+    });
+  }
 };
