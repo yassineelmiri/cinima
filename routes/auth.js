bbef9e5b4dfe56d5a6656caac149a5afbaa9b6f5
SafaMansouri <miriyassine16@gmail.com>
2024-09-26 15:25:35 +0100

    Implement authentication routes for registration, login, and logout.

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const AuthController = require('../controllers/AuthController');
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.post('/logout', AuthController.logout); // Ajoutez cette ligne
+router.post('/logout', AuthController.logout);
+router.post('/forgot-password', AuthController.forgotPassword);
+router.post('/reset-password', AuthController.resetPassword);
 
 module.exports = router;
