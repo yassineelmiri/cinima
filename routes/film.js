48a3eed66badffbdb707195dfb4edc5c26cf835e
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:58 +0100

    Add routes for managing film operations including listing, adding, updating, and deleting films.

--- routes/film.js
+++ routes/film.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const FilmController = require('../controllers/FilmController');
-const authMiddleware = require('../middleware/auth');
+const FilmController = require("../controllers/FilmController");
+const authMiddleware = require("../middleware/auth");
 
-router.post('/', FilmController.addFilm);
-router.get('/', FilmController.getFilms);
-router.put('/:id', FilmController.updateFilm);
-router.delete('/:id', FilmController.deleteFilm);
+router.post("/", FilmController.addFilm);
+router.get("/", FilmController.getFilms);
+router.put("/:id", FilmController.updateFilm);
+router.delete("/:id", FilmController.deleteFilm);
 
 module.exports = router;
