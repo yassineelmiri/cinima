03c7991b0f83121a0bc55869fa65a5a5377178e9
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:24:00 +0100

    Add email configuration using Nodemailer for sending confirmation emails.

--- emailConfig.js
+++ emailConfig.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, // Prend la valeur de l'environnement
-    pass: process.env.EMAIL_PASS  // Prend la valeur de l'environnement
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS  
   },
 });
 
