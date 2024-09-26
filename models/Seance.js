018161d04ae19a35901ef63b9da6ec15061bfada
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:57 +0100

    Add Seance model schema to define session scheduling and associated salle and film.

--- models/Seance.js
+++ models/Seance.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const SeanceSchema = new mongoose.Schema({
-    film: { type: mongoose.Schema.Types.ObjectId, ref: 'Film', required: true },
-    salle: { type: mongoose.Schema.Types.ObjectId, ref: 'Salle', required: true },
-    horaire: { type: Date, required: true },
-    tarif: { type: Number, required: true }
+  film: { type: mongoose.Schema.Types.ObjectId, ref: "Film", required: true },
+  salle: { type: mongoose.Schema.Types.ObjectId, ref: "Salle", required: true },
+  horaire: { type: Date, required: true },
+  tarif: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Seance', SeanceSchema);
+module.exports = mongoose.model("Seance", SeanceSchema);
