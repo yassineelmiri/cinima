fdcf86bf7af36f3836e4732247a85a28f08b3e94
SafaMansouri <miriyassine16@gmail.com>
2024-09-26 15:20:54 +0100

    Add Reservation model with authentication and password hashing functionality.

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ReservationSchema = new mongoose.Schema({
-    client: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    seance: { type: mongoose.Schema.Types.ObjectId, ref: 'Seance', required: true },
-    places: { type: Number, required: true },
-    dateReservation: { type: Date, default: Date.now }
+const reservationSchema = new mongoose.Schema({
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "client",
+    required: true,
+  },
+  seance: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Seance",
+    required: true,
+  },
+  places: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Reservation', ReservationSchema);
+module.exports = mongoose.model("Reservation", reservationSchema);
