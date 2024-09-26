9610059ad1e3b2e06d88393352642ebe1793e830
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:56 +0100

    Add Film model schema to define film structure and fields in the database.

--- models/Film.js
+++ models/Film.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const filmSchema = new mongoose.Schema({
-    titre: { type: String, required: true },
-    description: { type: String, required: true },
-    duree: { type: Number, required: true },
-    genre: { type: String, required: true },
-    dateSortie: { type: Date, required: true }
+  titre: { type: String, required: true },
+  description: { type: String, required: true },
+  duree: { type: Number, required: true },
+  genre: { type: String, required: true },
+  dateSortie: { type: Date, required: true },
 });
 
-module.exports = mongoose.model('Film', filmSchema);
+module.exports = mongoose.model("Film", filmSchema);
