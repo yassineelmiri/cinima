cd527c8321abd807d8ffc25cc4b97eb99b0fea3e
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:59 +0100

    Add routes for managing reservations including create, retrieve, and cancel functionalities.

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const ReservationController = require("../controllers/ReservationController");
 const authMiddleware = require("../middleware/auth");
 
-// Utilisation du middleware d'authentification pour protéger les routes
 router.post("/", authMiddleware, ReservationController.makeReservation);
 router.delete("/:id", authMiddleware, ReservationController.cancelReservation);
 router.get("/", authMiddleware, ReservationController.getReservations);
