a8b33b1caae39722de49bca1944cadb226e0705b
yassineelmiri <yassireelhima@gmail.com>
2024-09-27 00:23:59 +0100

    Add routes for salle management including create, update, list, and delete operations.

--- routes/salle.js
+++ routes/salle.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const SalleController = require('../controllers/SalleController');
-const authMiddleware = require('../middleware/auth');
+const SalleController = require("../controllers/SalleController");
+const authMiddleware = require("../middleware/auth");
 
-router.post('/', SalleController.addSalle);
-router.get('/', SalleController.getSalles);
-router.put('/:id', SalleController.updateSalle);
-router.delete('/:id', SalleController.deleteSalle);
+router.post("/", SalleController.addSalle);
+router.get("/", SalleController.getSalles);
+router.put("/:id", SalleController.updateSalle);
+router.delete("/:id", SalleController.deleteSalle);
 
 module.exports = router;
